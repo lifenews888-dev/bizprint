c7eafcfae564cca4143ec3ce662f1b67b44ac2ca
bizprintpro-alt <bizprintpro@gmail.com>
2026-04-23 12:52:19 +0800

    feat(storefront): add files from commit 8421e56

--- backend/src/commission/commission.module.ts
+++ backend/src/commission/commission.module.ts
@@ -2,19 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommissionLog } from './commission.entity';
 import { SalesCommission } from './sales-commission.entity';
+import { DesignerRoyalty } from './designer-royalty.entity';
 import { CommissionService } from './commission.service';
 import { CommissionController } from './commission.controller';
 import { Vendor } from '../vendors/vendor.entity';
 import { OrderVendorGroup } from '../orders/entities/order-vendor-group.entity';
+import { OrderItem } from '../orders/entities/order-item.entity';
 import { Order } from '../orders/entities/order.entity';
 import { Referral } from '../referral/referral.entity';
+import { Template } from '../templates/template.entity';
 import { MailModule } from '../mail/mail.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { NotificationModule } from '../notifications/notification.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CommissionLog, SalesCommission, Vendor, OrderVendorGroup, Order, Referral]),
+    TypeOrmModule.forFeature([CommissionLog, SalesCommission, DesignerRoyalty, Vendor, OrderVendorGroup, OrderItem, Order, Referral, Template]),
     MailModule,
     WalletModule,
     NotificationModule,
