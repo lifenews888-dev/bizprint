613a2bbc923673f1cb83c29d13e27f7a86aac7b6
bizprintpro-alt <bizprintpro@gmail.com>
2026-06-12 16:11:19 +0800

    Add mobile PWA install prompt

---
+++ frontend/public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'bizprint-shell-v1'
+const SHELL_URLS = ['/', '/manifest.webmanifest', '/icons/bizprint-icon.svg']
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(SHELL_URLS))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', event => {
+  const request = event.request
+  if (request.method !== 'GET') return
+
+  event.respondWith(
+    fetch(request).catch(() => caches.match(request).then(response => response || caches.match('/')))
+  )
+})
