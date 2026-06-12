8eb29c3ac7a4c97aa6b54c1646a2426499db43ae
bizprintpro-alt <lifenews888@gmail.com>
2026-06-12 20:58:30 +0800

    Make business card editor free

--- frontend/app/business-cards/layout.tsx
+++ frontend/app/business-cards/layout.tsx
@@ -1,9 +1,3 @@
-import ProGate from '@/components/ProGate'
-
 export default function BusinessCardsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <ProGate feature="Нэрийн хуудас editor" description="Нэрийн хуудас editor ашиглахад Basic төлөвлөгөө шаардлагатай" plan="basic">
-      {children}
-    </ProGate>
-  )
+  return <>{children}</>
 }
